10b94723364a39211f13856172b922e17eb63dec
Nikhil Srivastava <nikhilsrivastava@Nikhils-Air.Dlink>
2020-11-07 13:48:40 +0530

    basic header component done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
-
+import Header from './Header';
 function App() {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
+    <div className="app">
+       <Header/>
+      {/* Header */}
+     
+      {/* Home */}
+
     </div>
   );
 }
