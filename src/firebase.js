cd67aa10c441c072c36d10c6888dcea8dd7ff642
Nikhil Srivastava <nikhilsrivastava@Nikhils-Air.Dlink>
2020-11-07 09:34:25 +0530

    initial clean up done

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAns0xicimnVHa05CmipVcEwYrmhm5RX-I",
+    authDomain: "ecommerce-e6c15.firebaseapp.com",
+    databaseURL: "https://ecommerce-e6c15.firebaseio.com",
+    projectId: "ecommerce-e6c15",
+    storageBucket: "ecommerce-e6c15.appspot.com",
+    messagingSenderId: "613721269188",
+    appId: "1:613721269188:web:5d92342960a72175e3ba91",
+    measurementId: "G-6030406EL1"
+  };
