a430f1d39cf40e9485716054ae02d7dca33cf276
Nikhil Srivastava <nikhilsrivastava@Nikhils-Air.Dlink>
2020-11-08 21:42:54 +0530

    authentication done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Checkout from './Checkout';
 import {BrowserRouter as Router,Switch, Route} from 'react-router-dom';
 import Login from './Login';
+import {auth} from './firebase';
+import { useStateValue } from './StateProvider';
 function App() {
+  const [{}, disptach] = useStateValue();
+  useEffect(()=>{
+    auth.onAuthStateChanged(authUser=>{
+      if(authUser){
+        disptach({
+          type: 'SET_USER',
+          user: authUser
+        })
+      }
+      else{
+        disptach({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  },[])
   return (
     <Router>
         <div className="app">
