d6b8fe17cdb616dd98f2665b89ab0864855d5719
Nikhil Srivastava <nikhilsrivastava@Nikhils-MacBook-Air.local>
2020-11-15 18:01:49 +0530

    fron-end almost done and backend started

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ import Login from './Login';
 import {auth} from './firebase';
 import { useStateValue } from './StateProvider';
 import Payment from './Payment';
+import {loadStripe} from '@stripe/stripe-js';
+import {Elements} from '@stripe/react-stripe-js';
+
+const promise = loadStripe('pk_test_P4WvSEvNk5hAZq7xDbSZOQml');
 function App() {
   const [{}, disptach] = useStateValue();
   useEffect(()=>{
@@ -39,7 +43,9 @@ function App() {
             </Route>
             <Route path="/payment">
               <Header/>
-              <Payment/>
+              <Elements stripe={promise}>
+                <Payment/>
+              </Elements>
             </Route>
             <Route path="/">
               <Header/>
